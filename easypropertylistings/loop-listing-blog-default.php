7196304f3b9de1f00c43b7da994f1a8c50ccce94
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-10-11 15:51:34 +1300

    Added hook to loop page to output correct divs

--- easypropertylistings/loop-listing-blog-default.php
+++ easypropertylistings/loop-listing-blog-default.php
@@ -12,31 +12,30 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 global $property;
+
 ?>
 
-<div class="listing-loop-wrapper">
-	<div class="listing-loop-item">
-		<div class="listing-loop-item-image">
-			<?php do_action('epl_property_archive_featured_image'); ?>
+<div class="listing-loop-item">
+	<div class="listing-loop-item-image">
+		<?php do_action('epl_property_archive_featured_image'); ?>
+	</div>
+	<div class="listing-loop-item-content">
+		<div class="loop-title">
+			<span><?php do_action('epl_property_heading'); ?></span>
 		</div>
-		<div class="listing-loop-item-content">
-			<div class="loop-title">
-				<span><?php do_action('epl_property_heading'); ?></span>
-			</div>
-			<div class="loop-sale-type">
-				<span><?php do_action('epl_property_price'); ?></span>
-			</div>
-			<div class="loop-address">
-				<span><?php do_action('epl_property_address'); ?></span>
-			</div>
-			<div class="loop-extras">
-				<span>4 Bedrooms</span>
-				<span>5 Bathrooms</span>
-			</div>
+		<div class="loop-sale-type">
+			<span><?php do_action('epl_property_price'); ?></span>
+		</div>
+		<div class="loop-address">
+			<span><?php do_action('epl_property_address'); ?></span>
+		</div>
+		<div class="loop-extras">
+			<span>4 Bedrooms</span>
+			<span>5 Bathrooms</span>
+		</div>
 
-			<div class="loop-button">
-				<a href="<?php the_permalink() ?>">View Property</a>
-			</div>
+		<div class="loop-button">
+			<a href="<?php the_permalink() ?>">View Property</a>
 		</div>
 	</div>
 </div>
