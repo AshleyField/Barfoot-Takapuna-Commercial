93390ce5d1adf7982b673466d34a00faee909bbc
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-10-15 20:56:54 +1300

    Optimised banner area

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
 get_header();
 
+$bannerimage_id = get_field('banner_image');
+
 ?>
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to our blog</h1>
-    <div class="page-banner__intro">
-      <p>Keep up with our latest news</p>
-    </div>
-  </div>  
+<div class="page-banner" style="background-image: url('<?php echo get_theme_file_uri('assets/images/banner-image-3.jpg'); ?>')">
+
+		<div class="page-banner-head">
+			<h1>Market Updates</h1>
+			<p class="page-banner-subheading">Keep up with our latest news</p>
+		</div>
 </div>
 
-<div class="container container--narrow page-section">
+<div class="blog-loop-container">
 
   <?php 
   while(have_posts()){
     the_post(); ?>
-    <div class="post-item">
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+    <div class="blog-loop-item">
+      <h2 class="blog-loop-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
 
-      <div class="metabox">
+      <div class="blog-loop-meta">
         <p>Posted by <?php the_author_posts_link(); ?> on <?php  the_time('j M y'); ?> in <?php echo get_the_category_list(', '); ?></p> 
       </div>
 
-      <div class="generic-content">
+      <div class="blog-loop-content">
         <?php the_excerpt(); ?>
         <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
       </div>
