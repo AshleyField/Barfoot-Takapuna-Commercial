7bce83d9e7ba0036dfe122d950c6cd63fb54ebb3
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-10-24 19:17:59 +1300

    Started/Mid way though footer, cleaned single blog post, changed all links to be correct blue colour.

--- footer.php
+++ footer.php
@@ -1,5 +1,31 @@
 		<footer>
-			<p>test</p>
+			<div class="footer-inner">
+				<div class="footer-left">
+					<div class="footer-logo">
+						<img src="<?php echo get_bloginfo('template_directory');?>/assets/images/Barfoot-and-Thompson-Logo-Large.jpg?>" alt="">
+					</div>
+					<div class="footer-quote">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quos eum provident, magni quaerat cum blanditiis error fuga consequuntur rem.</p>
+					</div>
+				</div>
+				<div class="footer-right-side">
+					<div class="footer-middle">
+						<h2>Contact Us</h2>
+						<p>Email</p>
+						<p>Address</p>
+						<p>Phone</p>
+					</div>
+					<div class="footer-right">
+						<h2>Follow Us</h2>
+						<div class="social-icons">
+							<a href=""><i class="fab fa-facebook-square"></i></a>
+							<a href=""><i class="fab fa-linkedin"></i></a>
+							<a href=""><i class="fab fa-instagram"></i></a>
+						</div>
+					</div>
+				</div>
+				
+			</div>
 			<div class="footer-by">
 				<span>Website By <a href="">Precision Web</a></span>
 			</div>
@@ -13,10 +39,6 @@
 	<!-- Scripts -->
 
 	<?php wp_footer(); ?>
-<!-- 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<script type="text/javascript" src="/minified/scripts.min.js"></script> -->
 
 </body>
 </html>
--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 	<div class="page-banner" style="background-image: url('<?php the_field('blog_main_image'); ?>')">
 
 		<div class="page-banner-head">
-			<h1><?php the_title();?></h1>
+			<h1><?php the_field('blog_short_title');?></h1>
 		</div>
 	</div>
 	
@@ -12,16 +12,20 @@
 
 		<?php while(have_posts()) {
 			the_post(); ?>
-
 		<div class="blog-post-single">
+			<h2 class="blog-post-title"><?php the_title(); ?></h2>
+
 			<div class="posted-by-single">
 				<span>
 					Posted by <?php the_author_posts_link(); ?> on <?php  the_time('j M y'); ?> in <?php echo get_the_category_list(', '); ?>
 				</span>
 			</div>
-			<p class="page-content-info">
+			<div class="back-to-all">
+				<a href="/market-updates">All Market Updates</a>
+			</div>
+			<div class="page-content-info">
 				<?php the_content(); ?>
-			</p>
+			</div>
 		</div>
 
 		<?php }?>
