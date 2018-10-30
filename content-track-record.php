f32b30e8887658211665c2268cc45e525f4103d3
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-10-30 22:17:43 +1300

    Completed track record page.

--- content-track-record.php
+++ content-track-record.php
@@ -1,21 +1,19 @@
+<?php 
+$terms = get_the_terms(get_the_ID(),'property_status');
+
+foreach($terms as $term){
+ ?>
+
 <div class="track-record-property">
 	<div class="track-record-image">
 		<img src="<?php the_field('main_image');?>" alt="">
 		<div class="record-sticker-container">
-			<img class="record-sticker" src="<?php echo (get_field('sale_type')=='Sold') ? get_bloginfo('template_directory').'/assets/images/sold-Sticker.png' : get_bloginfo('template_directory').'/assets/images/lease-Sticker.png' ?>" alt="">
+			<img class="record-sticker" src="<?php echo ($term->name == 'Sold') ? get_bloginfo('template_directory').'/assets/images/sold-Sticker.png' : get_bloginfo('template_directory').'/assets/images/lease-Sticker.png' ?>" alt="">
 		</div>
 		
 	</div>
 	<div class="track-record-content">
 		<div class="track-record-title">
-			<?php 
-			$terms = get_the_terms(get_the_ID(),'property_status');
-
-			foreach($terms as $term){
-					echo $term->name;
-			}
-
-			 ?>
 			<p><?php the_title(); ?></p>
 		</div>
 		<?php $address_show = get_field('display_address');
@@ -34,3 +32,4 @@
 	</div>
 	
 </div>
+<?php } ?>
